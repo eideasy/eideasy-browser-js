1685f47db91a0ece078880f91aa2b2c3518fae61
matsjoonas <mj@codelight.eu>
2021-05-31 21:34:23 +0300

    Finish POC

--- src/createAsiceContainer.js
+++ src/createAsiceContainer.js
@@ -1,8 +1,16 @@
 import JSZip from 'jszip';
 
-const createAsiceContainer = function createAsiceContainer() {
+const createAsiceContainer = function createAsiceContainer(fileData) {
+  console.log(fileData);
   const zip = new JSZip();
-  return zip.file('nested/hello.txt', 'Hello World\n');
+  zip.file('mimetype', 'application/vnd.etsi.asic-e+zip');
+  zip.file('META-INF/manifest.xml', `<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+    <manifest:manifest xmlns:manifest="urn:oasis:names:tc:opendocument:xmlns:manifest:1.0">
+      <manifest:file-entry manifest:full-path="/" manifest:media-type="application/vnd.etsi.asic-e+zip"/>
+      <manifest:file-entry manifest:full-path="${fileData.name}" manifest:media-type="application/octet-stream"/>
+    </manifest:manifest>`);
+  zip.file(fileData.name, fileData.content);
+  return zip;
 };
 
 export default createAsiceContainer;
--- src/index-development.js
+++ src/index-development.js
@@ -6,11 +6,33 @@ const dom = {
   buttonCreateAsice: document.querySelector('.js-createAsiceContainer'),
 };
 
+function getInitialFiles() {
+  return document.getElementById('initialFilesInput').files;
+}
+
+function getFileContent(file, settings) {
+  const reader = new FileReader();
+  reader.addEventListener('error', () => {
+    console.error(`Error occurred reading file: ${file.name}`);
+  });
+  reader.addEventListener('load', (e) => {
+    const blob = new Blob([e.target.result], { type: 'application/pdf;charset=utf-8' });
+    const container = createAsiceContainer({
+      name: file.name,
+      content: blob,
+    });
+    container.generateAsync({ type: 'blob' })
+      .then((containerBlob) => {
+        saveAs((containerBlob), 'hello.asice');
+      });
+  });
+
+  const result = reader.readAsArrayBuffer(file, 'UTF-8');
+}
+
 dom.buttonCreateAsice.addEventListener('click', async (e) => {
   e.preventDefault();
-  const container = createAsiceContainer();
-  container.generateAsync({ type: 'blob' })
-    .then((blob) => {
-      saveAs(blob, 'hello.asice');
-    });
+  const fileList = getInitialFiles();
+  console.log(fileList);
+  getFileContent(fileList[0]);
 });
