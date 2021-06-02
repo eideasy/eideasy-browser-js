fadafe8a15a280f3feec0440103157a3b711f4a2
matsjoonas <mj@codelight.eu>
2021-06-02 11:21:43 +0300

    Add demo html for adding signtaures to an existing container

---
+++ docs/.vuepress/components/AddingSignatureExistingContainer.vue
@@ -0,0 +1,77 @@
+<script>
+
+import createAsiceContainer from '../../../src/createAsiceContainer';
+
+const init = function init(rootElem) {
+  // rootElem is the parent html element of this demo
+  console.log(rootElem);
+}
+
+export default {
+  name: 'AddingSignatureExistingContainer'
+}
+</script>
+
+<template>
+  <div
+    ref="example"
+    class="example"
+  >
+    <div class="formRow">
+      <label for="containerInput">
+        ASiC-E container
+      </label>
+      <input
+        class="js-containerInput"
+        id="containerInput"
+        type="file"
+        multiple
+      >
+    </div>
+
+    <div class="formRow">
+      <label for="signature1">
+        XAdES signature
+      </label>
+      <textarea
+        name="signature1"
+        id="signature1"
+        rows="10"
+      ></textarea>
+    </div>
+
+    <div class="formRow">
+      <label for="signature2">
+        XAdES signature
+      </label>
+      <textarea
+        name="signature2"
+        id="signature2"
+        rows="10"
+      ></textarea>
+    </div>
+
+    <button class="js-createAsiceContainer">
+      Add signatures and download the container
+    </button>
+  </div>
+</template>
+
+
+<style scoped>
+  label {
+    display: block;
+    width: 100%;
+    margin-bottom: 5px;
+    font-weight: bold;
+    font-size: 16px;
+  }
+
+  textarea {
+    width: 100%;
+  }
+
+  .formRow {
+    margin-bottom: 20px;
+  }
+</style>
--- docs/guide/demos.md
+++ docs/guide/demos.md
@@ -1,6 +1,8 @@
 # Demos
 
 ## Creating an ASiC-E container
+In this demo, we let the end user pick some files and when they click on the "Create ASiC-E container" button,
+an ASiC-E container is created with those files included.
 
 <CreatingAsiceContainer/>
 
@@ -10,3 +12,15 @@
 
 ### html
 @[code{50-58} html](../.vuepress/components/CreatingAsiceContainer.vue)
+
+
+## Adding XAdES signatures to an existing container
+In this demo, we let the end-user choose an ASiC-E container and manually enter the XAdES signatures.
+(there are text inputs for only 2 signatures in this example to keep things simple, but you could as easily add
+more signatures than that).
+
+
+
+<AddingSignatureExistingContainer/>
+
+## Full flow
