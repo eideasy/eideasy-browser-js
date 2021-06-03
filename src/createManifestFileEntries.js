039ad1166f35c0e792d83f2d83077652ab51509c
matsjoonas <mj@codelight.eu>
2021-06-03 09:52:45 +0300

    Rename entriesString to entries

--- src/createManifestFileEntries.js
+++ src/createManifestFileEntries.js
@@ -4,12 +4,12 @@
  * @returns {string} manifest entries
  */
 const createManifestFileEntries = function createManifestFileEntries(files) {
-  let entriesString = '';
+  let entries = '';
   for (let i = 0; i < files.length; i += 1) {
     const f = files[i];
-    entriesString += `<manifest:file-entry manifest:full-path="${f.name}" manifest:media-type="application/octet-stream"/>`;
+    entries += `<manifest:file-entry manifest:full-path="${f.name}" manifest:media-type="application/octet-stream"/>`;
   }
-  return entriesString;
+  return entries;
 };
 
 export default createManifestFileEntries;
