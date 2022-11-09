fcd28f4556a388c3393b381c11dc96acaff26da5
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-11-09 19:18:46 +0200

    fix: use the correct media type when creating manifest file entries

--- src/createManifestFileEntries.js
+++ src/createManifestFileEntries.js
@@ -7,7 +7,8 @@ const createManifestFileEntries = function createManifestFileEntries(files) {
   let entries = '';
   for (let i = 0; i < files.length; i += 1) {
     const f = files[i];
-    entries += `<manifest:file-entry manifest:full-path="${f.name}" manifest:media-type="application/octet-stream"/>`;
+    console.log(f);
+    entries += `<manifest:file-entry manifest:full-path="${f.name}" manifest:media-type="${f.content.type}"/>`;
   }
   return entries;
 };
